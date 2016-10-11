e53703366972ed866667652d1b20a5eb50ef7615
Christoph Rissner <resi@rissner.net>
2016-10-11 22:58:40 +0200

    add dumb devices

--- duw/device.py
+++ duw/device.py
@@ -17,6 +17,22 @@ DEV_ID_REG = 5000
 # sind überall identisch. Das ist eine Art regelmäßiges "Announcement" (oder
 # nenne es "Ping") hat vermutlich für dich keine Bedeutung.
 
+# 1158 -> SNR? 900.6666_00_TI_Modbus_Parameter_DE.pdf
+# 800 -> Summenstörung
+# 7500 -> Summenstörung 2
+
+# xx00 scheinen speziell addressen zu sein, da kommt keine antwort auf requests
+# lt 900.6660_01_TI_Modbus_RTU_DE.pdf könnten das die *dummen* RBGs
+# sein (RBG-X wird mit default adresse 100 angeführt)
+# dann wäre
+# 9120 das RBG-T (Touchpanel im Keller)
+# 9130 die Basisplatine Lüftung (im Keller)
+# xx50 VBOXen
+#
+# dafür spricht auch, dass 9120/9130 sich mit der gleichen dev-id (16,
+# zentralgerät) melden,
+# alle xx50er melden sich als dev-id 12 (vbox 120)
+
 class BaseHandler(object):
     def __init__(self, bus=None, active=True):
         self._active = threading.Event()
@@ -54,11 +70,17 @@ class DeviceCache(BaseHandler):
         super().__init__(*a, **kw)
         self.devices = defaultdict(dict)
 
+    def is_dumb_device(self, dev):
+        # see the header of this file about addresses
+        return bool(dev % 100)
+
     def inject(self, dev, reg, val=-1):
         cache = self.devices[dev]
         cache[reg] = (val, utcnow())
 
         # actively poll some values of interest
+        if self.is_dumb_device(dev):
+            return
         v = cache.get(DEV_ID_REG, None)
         if not v or (v[0] is None and (utcnow() - v[1]) > datetime.timedelta(seconds=10)):
             # make sure we're not notorious 'bout this request
