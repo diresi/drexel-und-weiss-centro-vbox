c37324ccadb01e8c60405e3af05c0439f849807c
Christoph Rissner <resi@rissner.net>
2020-11-18 21:33:08 +0100

    repo cleanup
    removed Python2 and some garbage

--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-alias V="source ${DIR}/venv/bin/activate"
-alias P="${DIR}/venv/bin/python"
--- logger.py
+++
@@ -1,5 +0,0 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-import duw as DUW
-DUW.Logger().start().join()
